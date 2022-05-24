30fdefc9fc27e8db0a44945b6d4f686cc8bfa74d
Rohan Kacheria <rohankacheriam@gmail.com>
2022-05-24 10:22:51 +0530

    reverse integer

---
+++ java/reverseinteger.java
@@ -0,0 +1,16 @@
+public class Solution {
+    /*
+    No need to consider sign
+    */
+    public int reverse(int x) {
+        long r = 0;
+        while (x != 0) {
+            r  = r * 10 + x % 10;
+            x /= 10;
+        }
+        if(r > Integer.MAX_VALUE || r < Integer.MIN_VALUE){
+            r = 0;
+        }    
+        return (int)r;
+    }
+}
